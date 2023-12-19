92216676798afe4ff9d8d269044950407dc9820a
Eduardo Vargas <eduardo704@gmail.com>
2023-12-19 12:54:06 -0300

    adding readme

--- apps/question1/src/app/user-profile/user-profile.component.ts
+++ apps/question1/src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserProfileService } from './user-profile.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
   userName: string;
 
   constructor(private userProfileService: UserProfileService) {
     this.userName = this.userProfileService.getUsername();
+  }
+  ngOnInit(): void {
+    console.log(this.userName)
+    // this.userProfileService.userName$
+    //   .pipe(take(1))
+    //   .subscribe((userName) => (this.userName = userName));
   }
 }
--- apps/question1/src/app/user-profile/user-profile.service.ts
+++ apps/question1/src/app/user-profile/user-profile.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
+  userName=new BehaviorSubject('Edu');
+  userName$=this.userName.asObservable();
 
   constructor() { }
 
   getUsername(){
     return 'Eduardo'
   }
+
+
+
+
 }
