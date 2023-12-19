04871e637b7fa48c18b736b80639d9eefd1f6d1c
Eduardo Vargas <eduardo704@gmail.com>
2023-12-19 06:47:05 -0300

    adding question 2 app

--- apps/question1/src/app/app.component.ts
+++ apps/question1/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { RouterModule } from '@angular/router';
 import { UserProfileModule } from './user-profile/user-profile.module';
 
 @Component({
- 
+  imports: [RouterModule, UserProfileModule],
   selector: 'blackthorn-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
